ccb1a541e369d603a9a07f2394dd13721a0df04c
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-27 22:46:52 +0100

    Update url to local

--- lib/src/infra/request.dart
+++ lib/src/infra/request.dart
@@ -4,7 +4,7 @@ import 'package:serve_rest_flutter_client/src/infra/response.dart';
 import 'package:http/http.dart' as http;
 
 class Request {
-  final String _basicUrl = 'https://serverest.dev/';
+  final String _basicUrl = 'http://10.0.2.2:3000'; //'https://serverest.dev/';
 
   Future<Response> get({required String path}) async {
     var response = await http.get(Uri.parse('$_basicUrl/$path'));
