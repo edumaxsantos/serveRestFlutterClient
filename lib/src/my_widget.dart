e5d65a1a66d2983e3e805a8b4e0044f23bca49e4
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-28 19:31:41 +0100

    use provider

--- lib/src/my_widget.dart
+++ lib/src/my_widget.dart
@@ -1,18 +1,23 @@
+import 'package:provider/provider.dart';
 import 'package:serve_rest_flutter_client/src/features/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:serve_rest_flutter_client/src/features/login/models/token_model.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => TokenModelNotifier(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
       ),
-      home: LoginPage(),
     );
   }
 }
