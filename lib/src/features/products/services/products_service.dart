c13f3d51659ca4e842232eb89c3d1cc547f566a5
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-28 21:53:06 +0100

    Add put()

--- lib/src/features/products/services/products_service.dart
+++ lib/src/features/products/services/products_service.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:serve_rest_flutter_client/src/features/products/models/product_model.dart';
 import 'package:serve_rest_flutter_client/src/features/products/models/products_model.dart';
 import 'package:serve_rest_flutter_client/src/infra/request.dart';
@@ -14,12 +15,36 @@ class ProductsService {
     throw Exception('Failed to load products');
   }
 
-  Future<Response> createProduct(ProductModel product, String token) async {
+  Future<Response> createProduct({
+    required ProductModel product,
+    required String token,
+  }) async {
     final response = await request.post(
       path: 'produtos',
       body: product.toJson(),
-      headers: {'Authorization': token},
+      headers: {
+        HttpHeaders.authorizationHeader: token,
+      },
     );
     return response;
+  }
+
+  Future<Response> deleteProduct({
+    required ProductModel product,
+    required String token,
+  }) async {
+    return await request.delete(path: 'produtos/${product.id}', headers: {
+      HttpHeaders.authorizationHeader: token,
+    });
+  }
+
+  Future<Response> editProduct(
+      {required ProductModel product, required String token}) async {
+    final productWithoutId = {...product.toJson()};
+    productWithoutId.remove('id');
+    return await request
+        .put(path: 'produtos/${product.id}', body: productWithoutId, headers: {
+      HttpHeaders.authorizationHeader: token,
+    });
   }
 }
