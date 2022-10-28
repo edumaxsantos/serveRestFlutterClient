97d7a5425311a5312dd5370fe2af2458bce54134
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-28 19:29:23 +0100

    create shared models and widgets

---
+++ lib/src/shared/widgets/basic_input_field_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BasicInputFieldWidget extends StatelessWidget {
+  final void Function(String)? onChanged;
+  final String label;
+  final bool isPassword;
+  final TextInputType textInputType;
+  const BasicInputFieldWidget({
+    super.key,
+    required this.onChanged,
+    required this.label,
+    this.isPassword = false,
+    this.textInputType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: onChanged,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+      ),
+      obscureText: isPassword,
+      keyboardType: textInputType,
+    );
+  }
+}
