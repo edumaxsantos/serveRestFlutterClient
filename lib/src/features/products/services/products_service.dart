7d1cfb89bbf5a4a825788da0fada37f90cc523a5
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-28 19:32:58 +0100

    add createProduct()

--- lib/src/features/products/services/products_service.dart
+++ lib/src/features/products/services/products_service.dart
@@ -1,14 +1,25 @@
+import 'package:serve_rest_flutter_client/src/features/products/models/product_model.dart';
 import 'package:serve_rest_flutter_client/src/features/products/models/products_model.dart';
 import 'package:serve_rest_flutter_client/src/infra/request.dart';
+import 'package:serve_rest_flutter_client/src/infra/response.dart';
 
 class ProductsService {
+  final Request request = Request();
   Future<ProductsModel> fetchProducts() async {
-    final response = await Request().get(path: 'produtos');
+    final response = await request.get(path: 'produtos');
     if (response.statusCode == 200) {
       final products = ProductsModel.fromJson(response.body!);
       return products;
-    } else {
-      throw Exception('Failed to load products');
     }
+    throw Exception('Failed to load products');
+  }
+
+  Future<Response> createProduct(ProductModel product, String token) async {
+    final response = await request.post(
+      path: 'produtos',
+      body: product.toJson(),
+      headers: {'Authorization': token},
+    );
+    return response;
   }
 }
