3e6d143172276b889c3b9b19a7fb9523d8dfb1e2
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2022-10-28 21:52:15 +0100

    add show snackbar

--- lib/src/shared/widgets/basic_input_field_widget.dart
+++ lib/src/shared/widgets/basic_input_field_widget.dart
@@ -5,17 +5,20 @@ class BasicInputFieldWidget extends StatelessWidget {
   final String label;
   final bool isPassword;
   final TextInputType textInputType;
+  final String? initialValue;
   const BasicInputFieldWidget({
     super.key,
     required this.onChanged,
     required this.label,
     this.isPassword = false,
     this.textInputType = TextInputType.text,
+    this.initialValue,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      initialValue: initialValue,
       onChanged: onChanged,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       decoration: InputDecoration(
